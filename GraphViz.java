ea9b7f8a052903327c83cc4dcb5e2e0c404b53f4
HAO ZHONG <547783993@qq.com>
2017-10-14 12:52:22 +0800

    B2 edit

--- GraphViz.java
+++ GraphViz.java
@@ -1,5 +1,5 @@
 package lab1;
-4
+2
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 package lab1;
-4
+2
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
